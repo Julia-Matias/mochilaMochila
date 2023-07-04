73a67d1d1a2f94bc1b5ad58371087b97be54c9c6
Julia Matias Reis <jmatiare@emeal.nttdata.com>
2023-07-04 14:08:55 -0300

    conclusao projeto

--- main.js
+++ main.js
@@ -1,29 +1,69 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || []; // verifica se o array possui dados e os recupera senão cria array vazio
+
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
 
 form.addEventListener("submit", (eventoEnviar) => {
   eventoEnviar.preventDefault();
-  //   (eventoEnviar.target[0].value); - busca pela dado de acordo posição dentro do array
-  //   (eventoEnviar.target.elements["nome"].value); - busca pelo dado de acordo com a tag dentro do objeto
+  const nome = eventoEnviar.target.elements["nome"];
+  const quantidade = eventoEnviar.target.elements["quantidade"];
+  const verificaItem = itens.find((elemento) => elemento.nome === nome.value);
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
 
-  criaElemento(
-    eventoEnviar.target.elements["nome"].value,
-    eventoEnviar.target.elements["quantidade"].value
-  );
+  if (verificaItem) {
+    itemAtual.id = verificaItem.id;
+    atualizaElemento(itemAtual);
+    itens[itens.findIndex((elemento) => elemento.id === verificaItem.id)] =
+      itemAtual;
+  } else {
+    itemAtual.id = itens[itens.length - 1] ? itens[itens.length - 1].id + 1 : 0;
+    criaElemento(itemAtual);
+    itens.push(itemAtual);
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens)); // o localstorage só lê texto, então usar o metodo JSON.stringifi para realizar a conversão
+  (nome.value = ""), (quantidade.value = "");
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
   const novoItem = document.createElement("li"); // cria um novo elemento do tipo lista
   novoItem.classList.add("item"); // informa que o novo elemento é semelhante aos da classe 'item'
 
   const quantidadeItem = document.createElement("strong");
-  quantidadeItem.innerHTML = quantidade;
+  quantidadeItem.innerHTML = item.quantidade;
+  quantidadeItem.dataset.id = item.id;
 
   novoItem.appendChild(quantidadeItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += item.nome;
+  novoItem.appendChild(botaoDeletar(item.id));
   lista.appendChild(novoItem);
-  console.log(novoItem);
 }
 
-// innerHTML - cria um novo objeto
-// appendChild - cria um filho para um objeto já criado
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
+}
+
+function botaoDeletar(id) {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
+  elementoBotao.addEventListener("click", function () {
+    deletarElemento(this.parentNode, id);
+  });
+  return elementoBotao;
+}
+
+function deletarElemento(tag, id) {
+  tag.remove();
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+  localStorage.setItem("itens", JSON.stringify(itens));
+}
